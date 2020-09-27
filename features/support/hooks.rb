04f6df2bbcbcd11073e8b79049dde46ab0e9213a
Lays <lays.lima@compasso.com.br>
2020-09-27 12:31:47 -0300

    inclusão dos testes de api

--- features/support/hooks.rb
+++
@@ -1,9 +0,0 @@
-After do |scenario|
-  scenario_name = scenario.name.gsub(/\s+/, '_').tr('/', '_')
-
-  if scenario.failed?
-    take_screenshot(scenario_name.downcase!, 'failed') 
-  else
-    take_screenshot(scenario_name.downcase!, 'success')
-  end
-end
